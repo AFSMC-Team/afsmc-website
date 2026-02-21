719d3eaefd8f40c3505d5e98ea1af69275e5423e
adnogpu <86664074+adnogpu@users.noreply.github.com>
2026-02-21 21:38:22 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,3 @@
-// vue.config.js
 module.exports = {
-  transpileDependencies: ['vue-router'],  // 强制转译 vue-router
-
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          include: /node_modules[\\/]vue-router/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              plugins: [
-                '@babel/plugin-proposal-optional-chaining'  // 直接用字符串，Vue CLI 默认有这个插件
-              ]
-            }
-          }
-        }
-      ]
-    }
-  }
+  transpileDependencies: ['vue-router']
 };
